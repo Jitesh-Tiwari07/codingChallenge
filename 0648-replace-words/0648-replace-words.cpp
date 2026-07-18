a903619a34954a23b1b82365c8f1edc920f5a1c9
Jitesh-Tiwari07 <tiwarijitesh315@gmail.com>
2026-07-18 12:41:04 +0530

    Time: 26 ms (85.02%), Space: 77.9 MB (39.06%) - LeetHub

--- 0648-replace-words/0648-replace-words.cpp
+++ 0648-replace-words/0648-replace-words.cpp
@@ -15,7 +15,6 @@ public:
 
         return newNode;
     }
-
     trieNode* root;
 
     Solution() {
@@ -24,36 +23,28 @@ public:
 
     void insert(string word) {
         trieNode* crawler = root;
-
         for (char ch : word) {
             int idx = ch - 'a';
 
             if (crawler->children[idx] == NULL) {
                 crawler->children[idx] = getNode();
             }
-
             crawler = crawler->children[idx];
         }
-
         crawler->isEndOfWord = true;
     }
 
     string searchRoot(string word) {
         trieNode* crawler = root;
-        string ans = "";
 
-        for (char ch : word) {
-            int idx = ch - 'a';
-
+        for (int i=0;i<word.length();i++) {
+            int idx = word[i] - 'a';
             if (crawler->children[idx] == NULL) {
                 return word;
             }
-
-            ans += ch;
             crawler = crawler->children[idx];
-
             if (crawler->isEndOfWord) {
-                return ans;
+                return word.substr(0,i+1);
             }
         }
 
@@ -63,15 +54,15 @@ public:
     string replaceWords(vector<string>& dictionary, string sentence) {
 
         
-        for (string &rootWord : dictionary) {
-            insert(rootWord);
+        for (string &word : dictionary) {
+            insert(word);
         }
 
         stringstream ss(sentence);
         string word;
         string result = "";
 
-        while (ss >> word) {                //while(getline(ss,word,' '))
+        while(getline(ss,word,' ')){                
             result += searchRoot(word)+" ";
         }
 
