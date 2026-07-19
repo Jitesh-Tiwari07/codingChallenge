be266e393c93d10bd85575f12ed562d22253935f
Jitesh-Tiwari07 <tiwarijitesh315@gmail.com>
2026-07-20 01:06:44 +0530

    Time: 0 ms (100%), Space: 9.8 MB (26.05%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<char,int>mp;
+        for(int i=0;i<s.length();i++){
+            mp[s[i]]++;
+        }
+        for(int i=0;i<t.size();i++){
+            mp[t[i]]--;
+        }
+        for(auto it: mp){
+            if(it.second!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
